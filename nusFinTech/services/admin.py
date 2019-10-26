dd6a4059b31a2266fc2c6d2d76979b63a9d5a689
antoni zhu <46621937+antonizhu@users.noreply.github.com>
2019-10-26 18:26:11 +0700

    ETF and ETF Price History db model

--- nusFinTech/services/admin.py
+++ nusFinTech/services/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from services.models import ETF, ETFHistory
 
 # Register your models here.
+admin.site.register(ETF)
+admin.site.register(ETFHistory)
--- nusFinTech/services/models.py
+++ nusFinTech/services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ETF(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class ETFHistory(models.Model):
+    etf = models.ForeignKey(ETF, on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return "{0} {1} {2}".format(self.etf, str(self.date), str(self.price))
