2f68bcdd79d267e896c3368a53d579edffb6f37d
antoni zhu <46621937+antonizhu@users.noreply.github.com>
2019-10-27 10:57:28 +0700

    Add link to the main page

--- nusFinTech/services/urls.py
+++ nusFinTech/services/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from services import views
+
+app_name = 'services'
 urlpatterns = [
     path('', views.index, name='index'),
     path('makeTransaction', views.makeTransaction, name='makeTransaction'),
