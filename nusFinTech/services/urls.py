0170b107aad64f5d71d2ab657a9e44f8190b9cb3
antoni zhu <46621937+antonizhu@users.noreply.github.com>
2019-11-03 20:29:07 +0700

    Add feature to calculate profit if application is not turned on for many days

--- nusFinTech/services/urls.py
+++ nusFinTech/services/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from services import views
-
+from services.models import ETF, ETFHistory, Account, AccountTransaction, AccountSummary
+from datetime import datetime, timedelta
+import random
 app_name = 'services'
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,3 +13,55 @@ urlpatterns = [
     path('yearlyReport', views.yearlyReport, name='yearlyReport'),
     path('withdraw', views.withdraw, name='withdraw'),
 ]
+
+def updateETFHistory(nForward=0):
+    print('updating etf history from last update to today...')
+    etfs = ETF.objects.all()
+
+    riskLevel = 0
+    for etf in etfs:
+        riskLevel += 1
+        etf_history = etf.history.order_by('-date').first()
+        print(etf_history)
+        today_date = datetime.now().date()
+        day_diff = today_date - etf_history.date
+        print('{0} days to patch ETF History!'.format(day_diff.days))
+        if day_diff.days > 0:
+            for aDate in (etf_history.date + timedelta(days=n) for n in range(1, day_diff.days+1+nForward)):
+                delta = riskLevel * random.randint(20, 40)/10000
+                ETFHistory.objects.get_or_create(etf=etf, date=aDate, delta=delta)
+
+
+def updateTransactionProfit(nForward=0):
+    print('updating Transaction Profit history from last update to today...')
+    accounts = Account.objects.all()
+    for account in accounts:
+        etfHistory = account.selectedETF.history.order_by('date')
+        profit_txns = list(filter(lambda txn: txn.type == AccountTransaction.TYPEPROFIT or txn.type == AccountTransaction.TYPELOSS, account.transactions.order_by('dateTime')))
+        lastProfitDate = profit_txns[-1].dateTime.date()
+        today_date = datetime.today().date()
+
+        day_diff = today_date - lastProfitDate
+        print('{0} days to patch Account Profits'.format(str(day_diff.days)))
+        if day_diff.days > 0:
+            for aDate in (lastProfitDate + timedelta(days=n) for n in range(1, day_diff.days+1+nForward)):
+                account_summary = AccountSummary(account)
+                that_day_txns = list(filter(lambda txn: txn.transactionDate == aDate, account_summary.transactionLedger))
+                that_day_etf_history = list(filter(lambda etfh: etfh.date == aDate, etfHistory))
+                
+                balance = account_summary.transactionLedger[-1].balance
+                if len(that_day_txns) != 0:
+                    balance = that_day_txns[0].balance
+                
+                delta = etfHistory.last().delta
+                if len(that_day_etf_history) != 0:
+                    delta = that_day_etf_history[0].delta
+                
+                profit = balance * delta
+
+                type = AccountTransaction.TYPEPROFIT if profit > 0 else AccountTransaction.TYPELOSS
+                AccountTransaction.objects.get_or_create(account=account, amount=profit, dateTime=aDate, type=type)
+
+updateETFHistory()
+
+updateTransactionProfit()
