a12d3e0ad7c5402bd4802310fb97b39b0bb912ef
antoni zhu <46621937+antonizhu@users.noreply.github.com>
2020-01-21 21:46:11 +0700

    accidental typo

--- nusFinTech/nusFinTech/settings.py
+++ nusFinTech/nusFinTech/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import logging.confidg
+import logging.config
 from django.utils.log import DEFAULT_LOGGING
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
